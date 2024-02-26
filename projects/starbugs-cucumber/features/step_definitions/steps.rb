a65447016301e03915e11f44e651b3780e71305e
davi rocha <liviamarconi31@gmail.com>
2024-02-25 21:48:17 -0300

    Curso BDD

---
+++ projects/starbugs-cucumber/features/step_definitions/steps.rb
@@ -0,0 +1,8 @@
+Quando('acesso a página principal da Starbugs') do
+    visit 'https://starbugs.vercel.app/'
+    sleep 10
+end
+  
+Então('eu devo ver uma lista de cafés disponíveis') do
+    puts 'trolei'
+end
